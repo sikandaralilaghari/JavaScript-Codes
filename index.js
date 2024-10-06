a27927d646cf90ce7557142e13162c76db5f9e24
sikandaralilaghari <112773125+sikandaralilaghari@users.noreply.github.com>
2024-10-05 23:53:42 -0700

    FInd Largest Number in the array using reduce method

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ console.log(`$${total}`);
 function sum(accumulation, element){
     return accumulation + element;
 }
+
+// Again use reduce method to find a largest number in the array
+
+const grades = [70,75,65,87,85];
+
+const maxScore = grades.reduce(getMax);
+console.log(`Maximum Score is: ${maxScore}`)
+function getMax(accumulator, element){
+    return Math.max(accumulator,element);
+}
